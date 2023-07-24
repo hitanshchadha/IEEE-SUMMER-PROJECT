c265799a549cffe23aa484323e7449535a10c658
Devanshgoel-123 <f20220457@pilani.bits-pilani.ac.in>
2023-07-24 14:42:50 +0530

    some more chnages

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -24,8 +24,8 @@ function Details(){
         
         <input  className="textbox" name="title" type="text" placeholder="Enter Title" />
         <input className="textbox" name="location" type="text" placeholder="Enter Location"  />
-        <input className="textbox" name ="time" type="text" placeholder="Enter Date and Time"  />
-        <input  className="textbox" name ="contact" type="text" placeholder="Enter Phone Number"  />
+        <input className="textbox" name ="time" type="date" placeholder="Enter Date and Time"  />
+        <input  className="textbox" name ="contact" type="tel" placeholder="Enter Phone Number"  />
         <input style={{height:"105px"}} className="textbox" name="description" type="text" placeholder="Description"  />
         
         </div>
